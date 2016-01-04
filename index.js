dde19730a76efb4525f7f06ace95df859fe0ad5d
euglazer <eugenestapler@gmail.com>
2016-01-04 16:59:17 +1300

    turn layers module into a stampit object

--- index.js
+++ index.js
@@ -3,33 +3,26 @@ var _ = require('lodash')
 function log (string) { console.log(string + ': ', eval(string)) }
 var layers = require('./js/layers')
 
-var $layerNum = $('#layer-no-value')
-
-// TODO: move to layers module
-var currentLayer = layers[0]
-
 $(document).on('keyup', function (e) {
   console.log('e.keyCode: ', e.keyCode)
 
-  // TODO: move to layers module
   if (_.inRange(e.keyCode, 49, 58)) {
     var layerNum = e.keyCode - 49
-    currentLayer = layers[layerNum]
-    $layerNum.text(layerNum + 1)
+    layers.setTo(layerNum)
   }
 
   switch (e.keyCode) {
     case 32: // enter
-      currentLayer.toggleDisplay(); break;
+      layers.current().toggleDisplay(); break;
     case 13: // space
-      currentLayer.toggleAnimation(); break;
+      layers.current().toggleAnimation(); break;
     case 39: // right arrow
-      currentLayer.changeEffect(true); break;
+      layers.current().changeEffect(true); break;
     case 37: // left arrow
-      currentLayer.changeEffect(false); break;
+      layers.current().changeEffect(false); break;
     case 38: // up arrow
-      currentLayer.changeStepLength(true); break;
+      layers.current().changeStepLength(true); break;
     case 40: // down arrow
-      currentLayer.changeStepLength(false); break;
+      layers.current().changeStepLength(false); break;
   }
 })
--- js/layers/index.js
+++ js/layers/index.js
@@ -1,23 +1,50 @@
 var $ = require('jquery')
+var _ = require('lodash')
 var stampit = require('stampit')
 var base =  require('./base')
 var horizontalLines = require('./horizontal-lines')
 var verticalLines = require('./vertical-lines')
 
-// need to adjust body size so that width and height are both a multiple of 5,
-// this ensures maximum subdivisions for patterns with repeating shapes
-// -- this will soon be a part of this layer's modules setup method
-var width = $('body').width()
-while (width % 12 !== 0) { width-- }
-$('body').width(width)
+var layers = stampit({
+  init: function () {
+    this.calibrateBody()
+    this.$layerValue = $('#layer-value')
+    this.$displayedValue = $('#displayed-value')
+    this.$animatedValue = $('#animated-value')
+    this.$speedValue = $('#speed-value')
+    this.$effectValue = $('#effect-value')
+    this.all = [
+      stampit.compose(horizontalLines, base)(),
+      stampit.compose(verticalLines, base)()
+    ]
+    this.setTo(0)
+  },
+  methods: {
+    setTo: function (layerNum) {
+      console.log(layerNum);
+      if (_.inRange(layerNum, 10)) {
+        this.layerNum = layerNum
+        this.updateIndicators()
+      }
+    },
+    current: function () {
+      return this.all[this.layerNum]
+    },
+    updateIndicators: function () {
+      this.$layerValue.text(this.layerNum + 1)
+      this.$displayedValue.text(this.current().isDisplayed)
+      this.$animatedValue.text(this.current().isMoving)
+      this.$speedValue.text(this.current().stepLength)
+    },
 
-var height = $('body').height()
-while (height % 12 !== 0) { height-- }
-$('body').height(height)
+    // private
+    calibrateBody: function () {
+      var width = $('body').width(), height = $('body').height()
+      while (width % 12 !== 0) { width-- }
+      while (height % 12 !== 0) { height-- }
+      $('body').css({width: width, height: height})
+    }
+  }
+})
 
-var layers = [
-  stampit.compose(horizontalLines, base)(),
-  stampit.compose(verticalLines, base)()
-]
-
-module.exports = layers
+module.exports = layers()
