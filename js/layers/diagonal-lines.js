bd7f8606702577b726c4b74a3011adb8b73d775d
euglazer <eugenestapler@gmail.com>
2016-01-04 22:40:07 +1300

    setup template for diagonal-lines

---
+++ js/layers/diagonal-lines.js
@@ -0,0 +1,26 @@
+var stampit = require('stampit')
+var divisors = require('array-math').divisors
+
+var diagonalLines = stampit({
+  init: function () {
+    this.description = 'diagonal lines'
+  },
+  methods: {
+    setup: function () {
+    },
+    generateShapes: function () {
+    },
+    printShape: function (shape, index) {
+    },
+    updateShapePosition: function (shape, index) {
+    },
+    increaseEffect: function () {
+    },
+    decreaseEffect: function () {
+    }
+
+    // private
+  }
+})
+
+module.exports = diagonalLines()
